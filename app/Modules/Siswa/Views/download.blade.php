b5833d462ef481bf61b17355763482935a74fcaa
masulfauzi <masul.fauzi@gmail.com>
2023-04-06 10:23:30 +0700

    ganti judul

--- app/Modules/Siswa/Views/download.blade.php
+++ app/Modules/Siswa/Views/download.blade.php
@@ -8,7 +8,7 @@
     <div class="page-title">
         <div class="row mb-2">
             <div class="col-12 col-md-6 order-md-1 order-last">
-                <h3>Manajemen Data {{ $title }}</h3>
+                <h3>Download Files</h3>
             </div>
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
