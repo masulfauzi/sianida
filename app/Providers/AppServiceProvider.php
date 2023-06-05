4c20db51b579e69ce05881ccba2e613963252a80
masulfauzi <masul.fauzi@gmail.com>
2023-06-05 12:54:18 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
