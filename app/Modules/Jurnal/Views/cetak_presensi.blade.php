4a28f82ab5b044d719aefdb9df62cdb37065710a
masulfauzi <masul.fauzi@gmail.com>
2023-09-07 14:00:33 +0700

    cetak daftar hadir kelas

--- app/Modules/Jurnal/Views/cetak_presensi.blade.php
+++ app/Modules/Jurnal/Views/cetak_presensi.blade.php
@@ -1,6 +1,6 @@
 @php
     header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
-    header("Content-Disposition: attachment; filename=abc.xls");  //File name extension was wrong
+    header("Content-Disposition: attachment; filename=daftar-hadir_kelas.xls");  //File name extension was wrong
     header("Expires: 0");
     header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
     header("Cache-Control: private",false);
