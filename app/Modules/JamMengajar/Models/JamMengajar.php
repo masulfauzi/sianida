1247ada26c34d62a8ff722e5a5ac4fe5024acfce
masulfauzi <masul.fauzi@gmail.com>
2023-10-12 10:25:14 +0700

    filter guru aktif

--- app/Modules/JamMengajar/Models/JamMengajar.php
+++ app/Modules/JamMengajar/Models/JamMengajar.php
@@ -57,6 +57,7 @@ public function kelas(){
 					->join('tingkat as e', 'd.id_tingkat','=','e.id')
 					->where('a.id_semester', $id_semester)
 					->where('a.id_guru', $id_guru)
+					->where('b.is_aktif', '1')
 					->groupBy('a.id_guru', 'e.id', 'c.id')
 					->orderBy('e.tingkat')
 					->orderBy('c.mapel')
