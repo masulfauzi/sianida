1541e14b616ebbca8f711c1b498f0db1af70fd31
masulfauzi <34361671+masulfauzi@users.noreply.github.com>
2024-06-29 08:43:48 +0700

    Create lihat_file.blade.php

---
+++ app/Modules/Siswa/Views/lihat_file.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{ url('uploads/'.$jenis.'/'.$file) }}">Klik untuk download file ini.</a>
+    <iframe
+    src="https://docs.google.com/gview?url={{ url('uploads/'.$jenis.'/'.$file) }}&embedded=true&toolbar=1"
+    style="width: 100%; 
+    height: 600px">
+    <p>Your browser does not support iframes.</p>
+</iframe>
+</body>
+</html>
