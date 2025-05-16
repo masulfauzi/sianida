e5338939549c6c8044a9a2b1b5dee95a22ef7614
masulfauzi <masul.fauzi@gmail.com>
2025-05-16 08:51:25 +0700

    fix: tambahkan fungsi popup

--- app/Modules/UjianSemester/Views/ujiansemester.blade.php
+++ app/Modules/UjianSemester/Views/ujiansemester.blade.php
@@ -109,6 +109,15 @@
 @endsection
 
 @section('page-js')
+    <script type="text/javascript">
+        // Popup window code
+        function newPopup(url) {
+            popupWindow = window.open(
+                url, 'popUpWindow',
+                'height=300,width=400,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes'
+            )
+        }
+    </script>
 @endsection
 
 @section('inline-js')
