a9a28694d419123daab005dcca6907a879ee21fe
Mas'ul <masul.fauzi@gmail.com>
2025-09-10 06:32:33 +0700

    fix: tampilkan respon API

--- app/Console/Commands/KirimWa.php
+++ app/Console/Commands/KirimWa.php
@@ -89,7 +89,7 @@ class KirimWa extends Command
                 $response = json_decode(curl_exec($curl));
 
                 curl_close($curl);
-                // echo $response;
+                echo $response;
             }
 
 
