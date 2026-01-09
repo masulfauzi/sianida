6799fa62143431c542b18b643d3ef3446ffb13b9
Mas'ul <masul.fauzi@gmail.com>
2026-01-09 07:06:35 +0700

    tampilkan siswa

--- app/Modules/Nilai/Controllers/NilaiController.php
+++ app/Modules/Nilai/Controllers/NilaiController.php
@@ -49,7 +49,7 @@ class NilaiController extends Controller
         $data['jurusan'] = Jurusan::all()->pluck('jurusan', 'id');
         $data['jurusan']->prepend('-PILIH SALAH SATU-', '');
 
-        $siswa = Siswa::select('siswas.*')
+        $siswa = Siswa::select('siswa.*')
             ->join('pesertadidik as p', 'siswa.id', '=', 'p.id_siswa')
             ->join('kelas as k', 'p.id_kelas', '=', 'k.id')
             ->join('tingkat as t', 'k.id_tingkat', '=', 't.id')
