b964a265f299ade729434af9610989a3faae4318
Mas'ul <masul.fauzi@gmail.com>
2026-01-20 13:32:35 +0700

    fix: hitung semua untuk finalisasi

--- app/Modules/Snbp/Models/Snbp.php
+++ app/Modules/Snbp/Models/Snbp.php
@@ -65,7 +65,8 @@ class Snbp extends Model
             ->where('k.id_jurusan', $id_jurusan)
             ->where('snbp.id_semester', $id_semester)
             ->where('p.id_semester', $id_semester)
-            ->whereNotNull('snbp.super')
+        // ->whereNotNull('snbp.super')
+            ->orderBy('snbp.super', 'DESC')
             ->orderBy('snbp.is_berminat', 'DESC')
             ->orderBy('snbp.total', 'DESC')
             ->get();
