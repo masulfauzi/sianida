aaf052e839e9bba2a76a9eb97ec0320ac0f0a79b
Mas'ul <masul.fauzi@gmail.com>
2026-01-21 08:37:55 +0700

    fix: tampilkan eligible final

--- app/Modules/Snbp/Views/snbp_siswa.blade.php
+++ app/Modules/Snbp/Views/snbp_siswa.blade.php
@@ -21,7 +21,7 @@
             </div>
         </div>
 
-        @if ($data->is_eligible_final != NULL)
+        @if ($data->is_eligible_final)
             <section class="section">
                 <div class="card">
                     <h6 class="card-header">
