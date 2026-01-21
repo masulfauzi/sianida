405ac127c078b003cf79a6730b21f65e3dadc86a
Mas'ul <masul.fauzi@gmail.com>
2026-01-21 19:38:28 +0700

    feat: buat urutan

--- app/Modules/Mapel/Controllers/MapelController.php
+++ app/Modules/Mapel/Controllers/MapelController.php
@@ -1,122 +1,145 @@
 <?php
 namespace App\Modules\Mapel\Controllers;
 
-use Form;
 use App\Helpers\Logger;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Modules\Log\Models\Log;
 use App\Modules\Mapel\Models\Mapel;
-
-use App\Http\Controllers\Controller;
+use Form;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MapelController extends Controller
 {
-	use Logger;
-	protected $log;
-	protected $title = "Mapel";
-	
-	public function __construct(Log $log)
-	{
-		$this->log = $log;
-	}
+    use Logger;
+    protected $log;
+    protected $title = "Mapel";
 
-	public function index(Request $request)
-	{
-		$query = Mapel::query();
-		if($request->has('search')){
-			$search = $request->get('search');
-			$query->where('mapel', 'like', "%$search%");
-		}
-		$data['data'] = $query->paginate(10)->withQueryString();
+    public function __construct(Log $log)
+    {
+        $this->log = $log;
+    }
 
-		$this->log($request, 'melihat halaman manajemen data '.$this->title);
-		return view('Mapel::mapel', array_merge($data, ['title' => $this->title]));
-	}
+    public function index(Request $request)
+    {
+        $query = Mapel::query();
+        if ($request->has('search')) {
+            $search = $request->get('search');
+            $query->where('mapel', 'like', "%$search%");
+        }
+        $data['data'] = $query->orderBy('urutan')->paginate(10)->withQueryString();
 
-	public function create(Request $request)
-	{
-		
-		$data['forms'] = array(
-			'mapel' => ['Mapel', Form::text("mapel", old("mapel"), ["class" => "form-control","placeholder" => ""]) ],
-			
-		);
+        $this->log($request, 'melihat halaman manajemen data ' . $this->title);
+        return view('Mapel::mapel', array_merge($data, ['title' => $this->title]));
+    }
 
-		$this->log($request, 'membuka form tambah '.$this->title);
-		return view('Mapel::mapel_create', array_merge($data, ['title' => $this->title]));
-	}
+    public function create(Request $request)
+    {
 
-	function store(Request $request)
-	{
-		$this->validate($request, [
-			'mapel' => 'required',
-			
-		]);
+        $data['forms'] = [
+            'mapel' => ['Mapel', Form::text("mapel", old("mapel"), ["class" => "form-control", "placeholder" => ""])],
 
-		$mapel = new Mapel();
-		$mapel->mapel = $request->input("mapel");
-		
-		$mapel->created_by = Auth::id();
-		$mapel->save();
+        ];
 
-		$text = 'membuat '.$this->title; //' baru '.$mapel->what;
-		$this->log($request, $text, ['mapel.id' => $mapel->id]);
-		return redirect()->route('mapel.index')->with('message_success', 'Mapel berhasil ditambahkan!');
-	}
+        $this->log($request, 'membuka form tambah ' . $this->title);
+        return view('Mapel::mapel_create', array_merge($data, ['title' => $this->title]));
+    }
 
-	public function show(Request $request, Mapel $mapel)
-	{
-		$data['mapel'] = $mapel;
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'mapel' => 'required',
 
-		$text = 'melihat detail '.$this->title;//.' '.$mapel->what;
-		$this->log($request, $text, ['mapel.id' => $mapel->id]);
-		return view('Mapel::mapel_detail', array_merge($data, ['title' => $this->title]));
-	}
+        ]);
 
-	public function edit(Request $request, Mapel $mapel)
-	{
-		$data['mapel'] = $mapel;
+        $mapel        = new Mapel();
+        $mapel->mapel = $request->input("mapel");
 
-		
-		$data['forms'] = array(
-			'mapel' => ['Mapel', Form::text("mapel", $mapel->mapel, ["class" => "form-control","placeholder" => "", "id" => "mapel"]) ],
-			
-		);
+        $mapel->created_by = Auth::id();
+        $mapel->save();
 
-		$text = 'membuka form edit '.$this->title;//.' '.$mapel->what;
-		$this->log($request, $text, ['mapel.id' => $mapel->id]);
-		return view('Mapel::mapel_update', array_merge($data, ['title' => $this->title]));
-	}
+        $text = 'membuat ' . $this->title; //' baru '.$mapel->what;
+        $this->log($request, $text, ['mapel.id' => $mapel->id]);
+        return redirect()->route('mapel.index')->with('message_success', 'Mapel berhasil ditambahkan!');
+    }
 
-	public function update(Request $request, $id)
-	{
-		$this->validate($request, [
-			'mapel' => 'required',
-			
-		]);
-		
-		$mapel = Mapel::find($id);
-		$mapel->mapel = $request->input("mapel");
-		
-		$mapel->updated_by = Auth::id();
-		$mapel->save();
+    public function show(Request $request, Mapel $mapel)
+    {
+        $data['mapel'] = $mapel;
 
+        $text = 'melihat detail ' . $this->title; //.' '.$mapel->what;
+        $this->log($request, $text, ['mapel.id' => $mapel->id]);
+        return view('Mapel::mapel_detail', array_merge($data, ['title' => $this->title]));
+    }
 
-		$text = 'mengedit '.$this->title;//.' '.$mapel->what;
-		$this->log($request, $text, ['mapel.id' => $mapel->id]);
-		return redirect()->route('mapel.index')->with('message_success', 'Mapel berhasil diubah!');
-	}
+    public function edit(Request $request, Mapel $mapel)
+    {
+        $data['mapel'] = $mapel;
 
-	public function destroy(Request $request, $id)
-	{
-		$mapel = Mapel::find($id);
-		$mapel->deleted_by = Auth::id();
-		$mapel->save();
-		$mapel->delete();
+        $data['forms'] = [
+            'mapel' => ['Mapel', Form::text("mapel", $mapel->mapel, ["class" => "form-control", "placeholder" => "", "id" => "mapel"])],
 
-		$text = 'menghapus '.$this->title;//.' '.$mapel->what;
-		$this->log($request, $text, ['mapel.id' => $mapel->id]);
-		return back()->with('message_success', 'Mapel berhasil dihapus!');
-	}
+        ];
+
+        $text = 'membuka form edit ' . $this->title; //.' '.$mapel->what;
+        $this->log($request, $text, ['mapel.id' => $mapel->id]);
+        return view('Mapel::mapel_update', array_merge($data, ['title' => $this->title]));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'mapel' => 'required',
+
+        ]);
+
+        $mapel        = Mapel::find($id);
+        $mapel->mapel = $request->input("mapel");
+
+        $mapel->updated_by = Auth::id();
+        $mapel->save();
+
+        $text = 'mengedit ' . $this->title; //.' '.$mapel->what;
+        $this->log($request, $text, ['mapel.id' => $mapel->id]);
+        return redirect()->route('mapel.index')->with('message_success', 'Mapel berhasil diubah!');
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $mapel             = Mapel::find($id);
+        $mapel->deleted_by = Auth::id();
+        $mapel->save();
+        $mapel->delete();
+
+        $text = 'menghapus ' . $this->title; //.' '.$mapel->what;
+        $this->log($request, $text, ['mapel.id' => $mapel->id]);
+        return back()->with('message_success', 'Mapel berhasil dihapus!');
+    }
+
+    public function orderUp(Request $request, Mapel $mapel)
+    {
+        if ($mapel->urutan > 1) {
+            $mapel->urutan     -= 1;
+            $mapel->updated_by  = Auth::id();
+            $mapel->save();
+
+            $text = 'mengurangi urutan ' . $this->title;
+            $this->log($request, $text, ['mapel.id' => $mapel->id]);
+            return back()->with('message_success', 'Urutan berhasil dikurangi!');
+        }
+
+        return back()->with('message_error', 'Urutan tidak bisa lebih kecil dari 1!');
+    }
+
+    public function orderDown(Request $request, Mapel $mapel)
+    {
+        $mapel->urutan     += 1;
+        $mapel->updated_by  = Auth::id();
+        $mapel->save();
+
+        $text = 'menambah urutan ' . $this->title;
+        $this->log($request, $text, ['mapel.id' => $mapel->id]);
+        return back()->with('message_success', 'Urutan berhasil ditambah!');
+    }
 
 }
--- app/Modules/Mapel/routes.php
+++ app/Modules/Mapel/routes.php
@@ -1,15 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Modules\Mapel\Controllers\MapelController;
+use Illuminate\Support\Facades\Route;
 
-Route::controller(MapelController::class)->middleware(['web','auth'])->name('mapel.')->group(function(){
-	Route::get('/mapel', 'index')->name('index');
-	Route::get('/mapel/data', 'data')->name('data.index');
-	Route::get('/mapel/create', 'create')->name('create');
-	Route::post('/mapel', 'store')->name('store');
-	Route::get('/mapel/{mapel}', 'show')->name('show');
-	Route::get('/mapel/{mapel}/edit', 'edit')->name('edit');
-	Route::patch('/mapel/{mapel}', 'update')->name('update');
-	Route::get('/mapel/{mapel}/delete', 'destroy')->name('destroy');
+Route::controller(MapelController::class)->middleware(['web', 'auth'])->name('mapel.')->group(function () {
+    Route::get('/mapel', 'index')->name('index');
+    Route::get('/mapel/data', 'data')->name('data.index');
+    Route::get('/mapel/create', 'create')->name('create');
+    Route::post('/mapel', 'store')->name('store');
+    Route::get('/mapel/{mapel}', 'show')->name('show');
+    Route::get('/mapel/{mapel}/edit', 'edit')->name('edit');
+    Route::patch('/mapel/{mapel}', 'update')->name('update');
+    Route::get('/mapel/{mapel}/delete', 'destroy')->name('destroy');
+    Route::get('/mapel/{mapel}/order/up', 'orderUp')->name('order.up.edit');
+    Route::get('/mapel/{mapel}/order/down', 'orderDown')->name('order.down.edit');
 });
