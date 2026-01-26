64c078dabdc618e3c84bed4b4c7a56c48e509ba3
Mas'ul <masul.fauzi@gmail.com>
2026-01-26 15:45:47 +0700

    fix: ubah max upload

--- app/Modules/SkMengajar/Controllers/SkMengajarController.php
+++ app/Modules/SkMengajar/Controllers/SkMengajarController.php
@@ -1,147 +1,145 @@
 <?php
 namespace App\Modules\SkMengajar\Controllers;
 
-use Form;
 use App\Helpers\Logger;
-use Illuminate\Http\Request;
-use App\Modules\Log\Models\Log;
-use App\Modules\SkMengajar\Models\SkMengajar;
-use App\Modules\Semester\Models\Semester;
-
 use App\Http\Controllers\Controller;
+use App\Modules\Log\Models\Log;
+use App\Modules\Semester\Models\Semester;
+use App\Modules\SkMengajar\Models\SkMengajar;
+use Form;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class SkMengajarController extends Controller
 {
-	use Logger;
-	protected $log;
-	protected $title = "SK Mengajar";
-	
-	public function __construct(Log $log)
-	{
-		$this->log = $log;
-	}
+    use Logger;
+    protected $log;
+    protected $title = "SK Mengajar";
 
-	public function index(Request $request)
-	{
-		$data['allow'] = [
-			'1fe8326c-22c4-4732-9c12-f7b83a16b842',
-			'bf1548f3-295c-4d73-809d-66ab7c240091'
-		];
+    public function __construct(Log $log)
+    {
+        $this->log = $log;
+    }
 
-		$query = SkMengajar::query();
-		if($request->has('search')){
-			$search = $request->get('search');
-			// $query->where('name', 'like', "%$search%");
-		}
-		$data['data'] = $query->paginate(10)->withQueryString();
+    public function index(Request $request)
+    {
+        $data['allow'] = [
+            '1fe8326c-22c4-4732-9c12-f7b83a16b842',
+            'bf1548f3-295c-4d73-809d-66ab7c240091',
+        ];
 
-		$this->log($request, 'melihat halaman manajemen data '.$this->title);
-		return view('SkMengajar::skmengajar', array_merge($data, ['title' => $this->title]));
-	}
+        $query = SkMengajar::query();
+        if ($request->has('search')) {
+            $search = $request->get('search');
+            // $query->where('name', 'like', "%$search%");
+        }
+        $data['data'] = $query->paginate(10)->withQueryString();
 
-	public function create(Request $request)
-	{
-		$ref_semester = Semester::all()->pluck('semester','id');
-		$ref_semester->prepend('-PILIH SALAH SATU-', '');
-		
-		$data['forms'] = array(
-			'id_semester' => ['Semester', Form::select("id_semester", $ref_semester, null, ["class" => "form-control select2"]) ],
-			'nama' => ['Nama', Form::text("nama", old("nama"), ["class" => "form-control","placeholder" => ""]) ],
-			'file' => ['File', Form::file("file", ["class" => "form-control","placeholder" => ""]) ],
-			
-		);
+        $this->log($request, 'melihat halaman manajemen data ' . $this->title);
+        return view('SkMengajar::skmengajar', array_merge($data, ['title' => $this->title]));
+    }
 
-		$this->log($request, 'membuka form tambah '.$this->title);
-		return view('SkMengajar::skmengajar_create', array_merge($data, ['title' => $this->title]));
-	}
+    public function create(Request $request)
+    {
+        $ref_semester = Semester::all()->pluck('semester', 'id');
+        $ref_semester->prepend('-PILIH SALAH SATU-', '');
 
-	function store(Request $request)
-	{
-		$this->validate($request, [
-			'id_semester' => 'required',
-			'nama' => 'required',
-			'file' => 'required|mimes:pdf,doc,docx|max:10240'
-			
-		]);
+        $data['forms'] = [
+            'id_semester' => ['Semester', Form::select("id_semester", $ref_semester, null, ["class" => "form-control select2"])],
+            'nama'        => ['Nama', Form::text("nama", old("nama"), ["class" => "form-control", "placeholder" => ""])],
+            'file'        => ['File', Form::file("file", ["class" => "form-control", "placeholder" => ""])],
 
-		$fileName = time().'.'.$request->file->extension();  
+        ];
+
+        $this->log($request, 'membuka form tambah ' . $this->title);
+        return view('SkMengajar::skmengajar_create', array_merge($data, ['title' => $this->title]));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'id_semester' => 'required',
+            'nama'        => 'required',
+            'file'        => 'required|mimes:pdf,doc,docx|max:102400',
+
+        ]);
+
+        $fileName = time() . '.' . $request->file->extension();
 
         $request->file->move(public_path('uploads/sk'), $fileName);
 
-		$skmengajar = new SkMengajar();
-		$skmengajar->id_semester = $request->input("id_semester");
-		$skmengajar->nama = $request->input("nama");
-		$skmengajar->file = $fileName;
-		
-		$skmengajar->created_by = Auth::id();
-		$skmengajar->save();
+        $skmengajar              = new SkMengajar();
+        $skmengajar->id_semester = $request->input("id_semester");
+        $skmengajar->nama        = $request->input("nama");
+        $skmengajar->file        = $fileName;
 
-		$text = 'membuat '.$this->title; //' baru '.$skmengajar->what;
-		$this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
-		return redirect()->route('skmengajar.index')->with('message_success', 'Sk Mengajar berhasil ditambahkan!');
-	}
+        $skmengajar->created_by = Auth::id();
+        $skmengajar->save();
 
-	public function show(Request $request, SkMengajar $skmengajar)
-	{
-		$data['skmengajar'] = $skmengajar;
+        $text = 'membuat ' . $this->title; //' baru '.$skmengajar->what;
+        $this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
+        return redirect()->route('skmengajar.index')->with('message_success', 'Sk Mengajar berhasil ditambahkan!');
+    }
 
-		$text = 'melihat detail '.$this->title;//.' '.$skmengajar->what;
-		$this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
-		return view('SkMengajar::skmengajar_detail', array_merge($data, ['title' => $this->title]));
-	}
+    public function show(Request $request, SkMengajar $skmengajar)
+    {
+        $data['skmengajar'] = $skmengajar;
 
-	public function edit(Request $request, SkMengajar $skmengajar)
-	{
-		$data['skmengajar'] = $skmengajar;
+        $text = 'melihat detail ' . $this->title; //.' '.$skmengajar->what;
+        $this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
+        return view('SkMengajar::skmengajar_detail', array_merge($data, ['title' => $this->title]));
+    }
 
-		$ref_semester = Semester::all()->pluck('semester','id');
-		
-		$data['forms'] = array(
-			'id_semester' => ['Semester', Form::select("id_semester", $ref_semester, null, ["class" => "form-control select2"]) ],
-			'nama' => ['Nama', Form::text("nama", $skmengajar->nama, ["class" => "form-control","placeholder" => "", "id" => "nama"]) ],
-			'file' => ['File', Form::text("file", $skmengajar->file, ["class" => "form-control","placeholder" => "", "id" => "file"]) ],
-			
-		);
+    public function edit(Request $request, SkMengajar $skmengajar)
+    {
+        $data['skmengajar'] = $skmengajar;
 
-		$text = 'membuka form edit '.$this->title;//.' '.$skmengajar->what;
-		$this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
-		return view('SkMengajar::skmengajar_update', array_merge($data, ['title' => $this->title]));
-	}
+        $ref_semester = Semester::all()->pluck('semester', 'id');
 
-	public function update(Request $request, $id)
-	{
-		$this->validate($request, [
-			'id_semester' => 'required',
-			'nama' => 'required',
-			'file' => 'required',
-			
-		]);
-		
-		$skmengajar = SkMengajar::find($id);
-		$skmengajar->id_semester = $request->input("id_semester");
-		$skmengajar->nama = $request->input("nama");
-		$skmengajar->file = $request->input("file");
-		
-		$skmengajar->updated_by = Auth::id();
-		$skmengajar->save();
+        $data['forms'] = [
+            'id_semester' => ['Semester', Form::select("id_semester", $ref_semester, null, ["class" => "form-control select2"])],
+            'nama'        => ['Nama', Form::text("nama", $skmengajar->nama, ["class" => "form-control", "placeholder" => "", "id" => "nama"])],
+            'file'        => ['File', Form::text("file", $skmengajar->file, ["class" => "form-control", "placeholder" => "", "id" => "file"])],
 
+        ];
 
-		$text = 'mengedit '.$this->title;//.' '.$skmengajar->what;
-		$this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
-		return redirect()->route('skmengajar.index')->with('message_success', 'Sk Mengajar berhasil diubah!');
-	}
+        $text = 'membuka form edit ' . $this->title; //.' '.$skmengajar->what;
+        $this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
+        return view('SkMengajar::skmengajar_update', array_merge($data, ['title' => $this->title]));
+    }
 
-	public function destroy(Request $request, $id)
-	{
-		$skmengajar = SkMengajar::find($id);
-		$skmengajar->deleted_by = Auth::id();
-		$skmengajar->save();
-		$skmengajar->delete();
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'id_semester' => 'required',
+            'nama'        => 'required',
+            'file'        => 'required',
 
-		$text = 'menghapus '.$this->title;//.' '.$skmengajar->what;
-		$this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
-		return back()->with('message_success', 'Sk Mengajar berhasil dihapus!');
-	}
+        ]);
+
+        $skmengajar              = SkMengajar::find($id);
+        $skmengajar->id_semester = $request->input("id_semester");
+        $skmengajar->nama        = $request->input("nama");
+        $skmengajar->file        = $request->input("file");
+
+        $skmengajar->updated_by = Auth::id();
+        $skmengajar->save();
+
+        $text = 'mengedit ' . $this->title; //.' '.$skmengajar->what;
+        $this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
+        return redirect()->route('skmengajar.index')->with('message_success', 'Sk Mengajar berhasil diubah!');
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $skmengajar             = SkMengajar::find($id);
+        $skmengajar->deleted_by = Auth::id();
+        $skmengajar->save();
+        $skmengajar->delete();
+
+        $text = 'menghapus ' . $this->title; //.' '.$skmengajar->what;
+        $this->log($request, $text, ['skmengajar.id' => $skmengajar->id]);
+        return back()->with('message_success', 'Sk Mengajar berhasil dihapus!');
+    }
 
 }
