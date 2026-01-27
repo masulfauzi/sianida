0989f12ef31c4e592a026ac917219e664bff1d21
Mas'ul <masul.fauzi@gmail.com>
2026-01-27 15:22:10 +0700

    debug: tampilkan tanpa middleware

--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/revoke-all-tokens', [AuthController::class, 'revokeAllTokens']);
 
     // Siswa API endpoints
-    Route::get('/siswa/{userId}', [SiswaController::class, 'siswa']);
 });
+
+Route::get('/siswa/{userId}', [SiswaController::class, 'siswa']);
