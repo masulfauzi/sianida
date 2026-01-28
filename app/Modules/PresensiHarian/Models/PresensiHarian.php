72ef6a3e36d11b4f2b640824084fae22798fa6ae
Mas'ul <masul.fauzi@gmail.com>
2026-01-28 20:48:15 +0700

    fix: ganti pake guarded

--- app/Modules/PresensiHarian/Models/PresensiHarian.php
+++ app/Modules/PresensiHarian/Models/PresensiHarian.php
@@ -1,29 +1,28 @@
 <?php
-
 namespace App\Modules\PresensiHarian\Models;
 
 use App\Helpers\UsesUuid;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Modules\Siswa\Models\Siswa;
 use App\Modules\StatusKehadiran\Models\StatusKehadiran;
-
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PresensiHarian extends Model
 {
-	use SoftDeletes;
-	use UsesUuid;
+    use SoftDeletes;
+    use UsesUuid;
 
-	protected $dates      = ['deleted_at'];
-	protected $table      = 'presensi_harian';
-	protected $fillable   = ['*'];	
+    protected $dates   = ['deleted_at'];
+    protected $table   = 'presensi_harian';
+    protected $guarded = ['id'];
 
-	public function siswa(){
-		return $this->belongsTo(Siswa::class,"id_siswa","id");
-	}
-public function statusKehadiran(){
-		return $this->belongsTo(StatusKehadiran::class,"id_status_kehadiran","id");
-	}
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class, "id_siswa", "id");
+    }
+    public function statusKehadiran()
+    {
+        return $this->belongsTo(StatusKehadiran::class, "id_status_kehadiran", "id");
+    }
 
 }
