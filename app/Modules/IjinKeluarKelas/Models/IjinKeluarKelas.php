7312df9624ebff8b0ccfd50992832838f8b8eeb2
Mas'ul <masul.fauzi@gmail.com>
2026-02-05 13:32:22 +0700

    fix: ubah fillable ke guarded

--- app/Modules/IjinKeluarKelas/Models/IjinKeluarKelas.php
+++ app/Modules/IjinKeluarKelas/Models/IjinKeluarKelas.php
@@ -1,41 +1,43 @@
 <?php
-
 namespace App\Modules\IjinKeluarKelas\Models;
 
 use App\Helpers\UsesUuid;
-use Illuminate\Support\Facades\DB;
+use App\Modules\Guru\Models\Guru;
+use App\Modules\IsValbk\Models\IsValbk;
+use App\Modules\IsValguru\Models\IsValguru;
+use App\Modules\JenisIjinKeluar\Models\JenisIjinKeluar;
+use App\Modules\Siswa\Models\Siswa;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Modules\Siswa\Models\Siswa;
-use App\Modules\Guru\Models\Guru;
-use App\Modules\JenisIjinKeluar\Models\JenisIjinKeluar;
-use App\Modules\IsValguru\Models\IsValguru;
-use App\Modules\IsValbk\Models\IsValbk;
-
 
 class IjinKeluarKelas extends Model
 {
-	use SoftDeletes;
-	use UsesUuid;
+    use SoftDeletes;
+    use UsesUuid;
 
-	protected $dates      = ['deleted_at'];
-	protected $table      = 'ijin_keluar_kelas';
-	protected $fillable   = ['*'];	
+    protected $dates   = ['deleted_at'];
+    protected $table   = 'ijin_keluar_kelas';
+    protected $guarded = ['id'];
 
-	public function siswa(){
-		return $this->belongsTo(Siswa::class,"id_siswa","id");
-	}
-public function guru(){
-		return $this->belongsTo(Guru::class,"id_guru","id");
-	}
-public function jenisIjinKeluar(){
-		return $this->belongsTo(JenisIjinKeluar::class,"id_jenis_ijin_keluar","id");
-	}
-public function isValguru(){
-		return $this->belongsTo(IsValguru::class,"is_valid_guru","id");
-	}
-public function isValbk(){
-		return $this->belongsTo(IsValbk::class,"is_valid_bk","id");
-	}
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class, "id_siswa", "id");
+    }
+    public function guru()
+    {
+        return $this->belongsTo(Guru::class, "id_guru", "id");
+    }
+    public function jenisIjinKeluar()
+    {
+        return $this->belongsTo(JenisIjinKeluar::class, "id_jenis_ijin_keluar", "id");
+    }
+    public function isValguru()
+    {
+        return $this->belongsTo(IsValguru::class, "is_valid_guru", "id");
+    }
+    public function isValbk()
+    {
+        return $this->belongsTo(IsValbk::class, "is_valid_bk", "id");
+    }
 
 }
