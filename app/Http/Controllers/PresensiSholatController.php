019506e289984f2cb6d56c294af4e5b1ac9813e5
Mas'ul <masul.fauzi@gmail.com>
2026-02-06 13:32:13 +0700

    fix: kembalikan query

--- app/Http/Controllers/PresensiSholatController.php
+++ app/Http/Controllers/PresensiSholatController.php
@@ -15,26 +15,9 @@ class PresensiSholatController extends Controller
     public function index(Request $request)
     {
         try {
-            $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $request->bulan, now()->year);
-
-            // Create array of days
-            $allDays = collect(range(1, $daysInMonth))->map(function ($day) {
-                return ['day' => $day, 'count' => 0];
-            })->keyBy('day');
-
-            // Get actual presensi data grouped by day
-            $presensiData = PresensiSholat::where('nisn', $request->nisn)
-                ->where('jenis_presensi', 'Sholat Dzuhur')
+            $data = PresensiSholat::where('nisn', $request->nisn)
                 ->whereMonth('Waktu_Presensi', $request->bulan)
-                ->selectRaw('DAY(Waktu_Presensi) as day, COUNT(*) as count')
-                ->groupBy(DB::raw('DAY(Waktu_Presensi)'))
-                ->get()
-                ->keyBy('day');
-            
-            // Merge data - show all days with presensi count or 0 if not present
-            $data = $allDays->map(function ($item, $day) use ($presensiData) {
-                return $presensiData->has($day) ? $presensiData[$day] : $item;
-            })->values();
+                ->get();
 
             return response()->json([
                 'success' => true,
