6363e5b9e0e8ba5fce51fdffa2e4d7f73fef8c23
Mas'ul <masul.fauzi@gmail.com>
2026-02-06 13:27:29 +0700

    feat: tambah data sejumlah hari di bulan

--- app/Http/Controllers/PresensiSholatController.php
+++ app/Http/Controllers/PresensiSholatController.php
@@ -3,7 +3,6 @@ namespace App\Http\Controllers;
 
 use App\Models\PresensiSholat;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class PresensiSholatController extends Controller
 {
@@ -15,12 +14,22 @@ class PresensiSholatController extends Controller
     public function index(Request $request)
     {
         try {
-            $data = PresensiSholat::where('nisn', $request->nisn)
+            $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $request->bulan, now()->year);
+
+            // Create array of days
+            $allDays = collect(range(1, $daysInMonth))->map(function ($day) {
+                return ['day' => $day, 'count' => 0];
+            })->keyBy('day');
+
+            // Get actual presensi data
+            $presensiData = PresensiSholat::where('nisn', $request->nisn)
                 ->where('jenis_presensi', 'Sholat Dzuhur')
                 ->whereMonth('Waktu_Presensi', $request->bulan)
-                ->selectRaw('DAY(Waktu_Presensi) as day, COUNT(*) as count')
-                ->groupBy(DB::raw('DAY(Waktu_Presensi)'))
                 ->get();
+            // Merge data
+            $data = $allDays->map(function ($item, $day) use ($presensiData) {
+                return $presensiData->has($day) ? $presensiData[$day] : $item;
+            })->values();
 
             return response()->json([
                 'success' => true,
