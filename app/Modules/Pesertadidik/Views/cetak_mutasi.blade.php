ac0065db79908d292dfbac8392f3687f251994cd
Mas'ul <masul.fauzi@gmail.com>
2026-05-18 13:07:55 +0700

    feat: ubah font menjadi Trebuchet MS

--- app/Modules/Pesertadidik/Views/cetak_mutasi.blade.php
+++ app/Modules/Pesertadidik/Views/cetak_mutasi.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cetak Halaman Mutasi</title>
+    <link href="https://fonts.cdnjs.com/css2?family=Trebuchet+MS" rel="stylesheet">
     <style>
         body {
-            font-family: Calibri;
+            font-family: 'Trebuchet MS', sans-serif;
         }
 
         .center {
